150e37dc67c454c51b8ec0a23d16e50f2ec69cd1
marcellita <mteliceskidalpino@gmail.com>
2020-09-11 17:57:57 -0300

    projeto final

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ let map, heatmap, marker, i;
 
 async function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 11,
     center: { lat: -23.5489, lng: -46.6388 },
     styles: [
       { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
@@ -11,130 +11,288 @@ async function initMap() {
       {
         featureType: "administrative.locality",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
+        stylers: [{ color: "#d59563" }],
       },
       {
         featureType: "poi",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
+        stylers: [{ color: "#d59563" }],
       },
       {
         featureType: "poi.park",
         elementType: "geometry",
-        stylers: [{ color: "#263c3f" }]
+        stylers: [{ color: "#263c3f" }],
       },
       {
         featureType: "poi.park",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }]
+        stylers: [{ color: "#6b9a76" }],
       },
       {
         featureType: "road",
         elementType: "geometry",
-        stylers: [{ color: "#38414e" }]
+        stylers: [{ color: "#38414e" }],
       },
       {
         featureType: "road",
         elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }]
+        stylers: [{ color: "#212a37" }],
       },
       {
         featureType: "road",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }]
+        stylers: [{ color: "#9ca5b3" }],
       },
       {
         featureType: "road.highway",
         elementType: "geometry",
-        stylers: [{ color: "#746855" }]
+        stylers: [{ color: "#746855" }],
       },
       {
         featureType: "road.highway",
         elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }]
+        stylers: [{ color: "#1f2835" }],
       },
       {
         featureType: "road.highway",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }]
+        stylers: [{ color: "#f3d19c" }],
       },
       {
         featureType: "transit",
         elementType: "geometry",
-        stylers: [{ color: "#2f3948" }]
+        stylers: [{ color: "#2f3948" }],
       },
       {
         featureType: "transit.station",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }]
+        stylers: [{ color: "#d59563" }],
       },
       {
         featureType: "water",
         elementType: "geometry",
-        stylers: [{ color: "#17263c" }]
+        stylers: [{ color: "#17263c" }],
       },
       {
         featureType: "water",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }]
+        stylers: [{ color: "#515c6d" }],
       },
       {
         featureType: "water",
         elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }]
-      }
-    ]
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
   });
-  const response = await consumirApi(endpoint, Appquery)
+  const region = {
+    sul: {
+      center: {
+        lat: -23.6542,
+        lng: -46.6592,
+      },
+    },
+    leste: {
+      center: {
+        lat: -23.5338,
+        lng: -46.5033,
+      },
+    },
+    oeste: {
+      center: {
+        lat: -23.5719,
+        lng: -46.7008,
+      },
+    },
+    norte: {
+      center: {
+        lat: -23.4803,
+        lng: -46.6708,
+      },
+    },
+    central: {
+      center: {
+        lat: -23.5489,
+        lng: -46.6388,
+      },
+    },
+  };
+
+  for (const zona in region) {
+    const cityCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map,
+      center: region[zona].center,
+      radius: Math.sqrt(2000) * 100,
+    });
+  }
+
+  function zoomZones({ lat, lng }) {
+    map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 13,
+      center: { lat, lng },
+      styles: [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#263c3f" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#6b9a76" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#38414e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#212a37" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#746855" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#1f2835" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#f3d19c" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [{ color: "#2f3948" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#d59563" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#17263c" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#515c6d" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#17263c" }],
+        },
+      ],
+    });
+    heatmap = new google.maps.visualization.HeatmapLayer({
+      data: getPoints,
+      map: map,
+    });
+  }
+
+  document
+    .getElementById("central-zone")
+    .addEventListener("click", () =>
+      zoomZones({ lat: -23.5489, lng: -46.6388 })
+    );
+  document
+    .getElementById("north-zone")
+    .addEventListener("click", () =>
+      zoomZones({ lat: -23.4803, lng: -46.6708 })
+    );
+  document
+    .getElementById("south-zone")
+    .addEventListener("click", () =>
+      zoomZones({ lat: -23.6542, lng: -46.6592 })
+    );
+  document
+    .getElementById("east-zone")
+    .addEventListener("click", () =>
+      zoomZones({ lat: -23.5338, lng: -46.5033 })
+    );
+  document
+    .getElementById("west-zone")
+    .addEventListener("click", () =>
+      zoomZones({ lat: -23.5719, lng: -46.7008 })
+    );
+
+  const response = await consumirApi(endpoint, Appquery);
   const getPoints = response.data.closestDrivers.drivers.map((item) => {
     if (item.busy === false) {
-      return new google.maps.LatLng(item.lat, item.lng)
+      return new google.maps.LatLng(item.lat, item.lng);
     }
-  })
+  });
 
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: getPoints,
-    map: map
+    map: map,
   });
   const countAllDrivers = response.data.closestDrivers.driversCount;
   const countReadyDrivers = response.data.closestDrivers.readyDriversCount;
   const countBusyDrivers = response.data.closestDrivers.busyDriversCount;
 
-const allDrivers = document.getElementById("allDrivers")
-allDrivers.innerHTML = countAllDrivers
-const busyDrivers = document.getElementById("busyDrivers")
-busyDrivers.innerHTML = countBusyDrivers
-const readyDrivers = document.getElementById("readyDrivers")
-readyDrivers.innerText = countReadyDrivers
+  const allDrivers = document.getElementById("all-drivers");
+  allDrivers.innerHTML = countAllDrivers;
+  const busyDrivers = document.getElementById("busy-drivers");
+  busyDrivers.innerHTML = countBusyDrivers;
+  const readyDrivers = document.getElementById("ready-drivers");
+  readyDrivers.innerText = countReadyDrivers;
 
   setMarkers(map);
-  var south = new google.maps.LatLng(-23.5489, -46.6388)
-  var east = new google.maps.LatLng(-23.5338, -46.5033)
-  var north = new google.maps.LatLng( -23.5719, -46.7008)
-  var central = new google.maps.LatLng( -23.5489, -46.6388)
-  var west = new google.maps.LatLng(-23.5719, -46.7008)
 }
 
 const region = [
-  ['Zona Sul', -23.6542, -46.6592, 4],
-  ['Zona Leste', -23.5338, -46.5033, 3],
-  ['Zona Oeste', -23.5719, -46.7008, 2],
-  ['Zona Norte', -23.4803, -46.6708, 1],
-  ['Zona Central', -23.5489, -46.6388, 0]
-]
+  ["Zona Sul", -23.6542, -46.6592, 4],
+  ["Zona Leste", -23.5338, -46.5033, 3],
+  ["Zona Oeste", -23.5719, -46.7008, 2],
+  ["Zona Norte", -23.4803, -46.6708, 1],
+  ["Zona Central", -23.5489, -46.6388, 0],
+];
 function setMarkers(map) {
   let image = {
-    url: './imagem/delivery-man.png',
+    url: "./imagem/delivery-man.png",
     size: new google.maps.Size(50, 50),
     origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(0, 32)
+    anchor: new google.maps.Point(0, 32),
   };
 
   let shape = {
     coords: [1, 1, 1, 40, 40, 40, 40, 1],
-    type: 'poly'
+    type: "poly",
   };
-  for (var x = 0; x < region.length; x++) {
+  for (let x = 0; x < region.length; x++) {
     let zona = region[x];
     marker = new google.maps.Marker({
       position: { lat: zona[1], lng: zona[2] },
@@ -142,26 +300,27 @@ function setMarkers(map) {
       icon: image,
       shape: shape,
       title: zona[0],
-      zIndex: zona[3]
+      zIndex: zona[3],
     });
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-        infowindow.setContent(zona[0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
+    google.maps.event.addListener(
+      marker,
+      "click",
+      (function (marker, i) {
+        return function () {
+          infowindow.setContent(zona[0]);
+          infowindow.open(map, marker);
+        };
+      })(marker, i)
+    );
   }
-  var infowindow = new google.maps.InfoWindow();
+  let infowindow = new google.maps.InfoWindow();
 }
 
-function toggleHeatmap() {
-  heatmap.setMap(heatmap.getMap() ? null : map);
-}
 const endpoint = `https://www.loggi.com/graphql`;
 
 const Appquery = `
 query AppQuery {
-  closestDrivers(productType: 2, transportType: "1", lat: -23.55, lng: -46.63, radius: 10.0, limit: 200, citySlug:"sp") {
+  closestDrivers(productType: 2, transportType: "1", lat: -23.55, lng: -46.63, radius: 15.0, limit: 400, citySlug:"sp") {
     driversCount
     readyDriversCount
     busyDriversCount
@@ -175,40 +334,29 @@ query AppQuery {
 
 const consumirApi = async (endpoint, query, variables = {}) => {
   const response = await fetch(endpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
       Authorization: `ApiKey larissa.miyaji@gmail.com:690606c72dd744f76826a04630a8a16268ac2560`,
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ query, variables })
+    body: JSON.stringify({ query, variables }),
   });
   return response.json();
-}
+};
 
-function changeGradient() {
-  const gradient = [
-    "rgba(0, 255, 255, 0)",
-    "rgba(0, 255, 255, 1)",
-    "rgba(0, 191, 255, 1)",
-    "rgba(0, 127, 255, 1)",
-    "rgba(0, 63, 255, 1)",
-    "rgba(0, 0, 255, 1)",
-    "rgba(0, 0, 223, 1)",
-    "rgba(0, 0, 191, 1)",
-    "rgba(0, 0, 159, 1)",
-    "rgba(0, 0, 127, 1)",
-    "rgba(63, 0, 91, 1)",
-    "rgba(127, 0, 63, 1)",
-    "rgba(191, 0, 31, 1)",
-    "rgba(255, 0, 0, 1)"
-  ];
-  heatmap.set("gradient", heatmap.get("gradient") ? null : gradient);
-}
 
-function changeRadius() {
-  heatmap.set("radius", heatmap.get("radius") ? null : 20);
-}
 
-function changeOpacity() {
-  heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
